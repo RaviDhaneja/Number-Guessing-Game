a3a84cb0273a2d1d4fafe88066be934f7064de86
RaviDhaneja <ravidhaneja12@gmail.com>
2024-02-03 17:25:35 +0530

    add number of guesses

--- Number-Guessing-Game.py
+++ Number-Guessing-Game.py
@@ -2,11 +2,14 @@ import random
 
 RandomNumber=random.randint(1,100)
 YourNumber=0
+count=0
 while YourNumber != RandomNumber:
     YourNumber=int(input("Enter a number between 1 and 100 :- "))
+    count+=1
     if (YourNumber < RandomNumber):
         print(" Too Low !")     
     elif (YourNumber>RandomNumber):
         print("Too high !")
     elif(YourNumber == RandomNumber):
         print("You Guessed it !")
+        print("Number of Guesses:- " + str(count))
