9427988a6444ea6d1a64b069aea5cdbc8476c1f9
RaviDhaneja <ravidhaneja12@gmail.com>
2024-03-11 12:11:55 +0530

    Update Rock-paper-scissor.py

--- Rock-paper-scissor.py
+++ Rock-paper-scissor.py
@@ -1,22 +1,23 @@
 import random
 
-user = input("Enter a Choice(rock,paper,scissors) :").lower()
-possible = ["rock", "paper", "scissors"]
+user_input=input("Enter a Choice(rock,paper,scissors) :").lower()
+user_text = user_input.lower()
+possible=["rock","paper","scissors"]
 computer_choice = random.choice(possible)
-print("your choice: " + user + " computer choice: " + computer_choice)
-if user == computer_choice or user == computer_choice:
-    print("Both players selected" + user + "It's a tie!")
-elif user == "rock":
+print("your choice: "+ user_text +  "  computer choice: "+computer_choice)
+if user_text == computer_choice or user_text == computer_choice:
+    print("Both players selected"+user_text+"It's a tie!")
+elif user_text == "rock":
     if computer_choice == "scissors":
         print("Rock smashes scissors! You win!")
     else:
         print("Paper covers rock! You lose.")
-elif user == "paper":
+elif user_text == "paper":
     if computer_choice == "rock":
         print("Paper covers rock! You win!")
     else:
         print("Scissors cuts paper! You lose.")
-elif user == "scissors":
+elif user_text == "scissors":
     if computer_choice == "paper":
         print("Scissors cuts paper! You win!")
     else:
